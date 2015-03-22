78c6226d8537e8c793fd185501ef297c6a78c14f
Marcus Bauer <mabako@gmail.com>
2015-03-23 00:06:15 +0100

    Initial Version

---
+++ Mod.cs
@@ -0,0 +1,51 @@
+﻿using ColossalFramework;
+using ColossalFramework.UI;
+using ICities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace HidePolicyIcons
+{
+    public class ModInfo : IUserMod
+    {
+
+        public string Description
+        {
+            get
+            {
+                return "Hides all the pesky district policy icons";
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return "Hide Policy Icons";
+            }
+        }
+    }
+
+    public class Hidden : LoadingExtensionBase
+    {
+        private UITextureAtlas atlas = null;
+
+        public override void OnLevelLoaded(LoadMode mode)
+        {
+            atlas = DistrictManager.instance.m_properties.m_areaIconAtlas;
+            DistrictManager.instance.m_properties.m_areaIconAtlas = null;
+            DistrictManager.instance.NamesModified();
+        }
+
+        public override void OnLevelUnloading()
+        {
+            if(atlas != null)
+                DistrictManager.instance.m_properties.m_areaIconAtlas = atlas;
+        }
+    }
+}
